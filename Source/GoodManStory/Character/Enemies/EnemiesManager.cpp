ecf143dff36703c1ce7288c942f40def909d7a81
Tristan <t.francillonne@student.isartdigital.com>
2020-10-02 15:58:34 +0200

    Add Manager enemies

---
+++ Source/GoodManStory/Character/Enemies/EnemiesManager.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemiesManager.h"
+#include "AIController.h"
+#include "TimerManager.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "BaseEnemy.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AEnemiesManager::AEnemiesManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemiesManager::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorldTimerManager().SetTimer(TimerActuMinionSpawn, this,&AEnemiesManager::Spawn, 0.2, true, 1.0f);
+	if(TrashMob)
+	{
+		SpawnParams.Owner = this;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	}
+
+}
+
+// Called every frame
+void AEnemiesManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEnemiesManager::Spawn()
+{
+	if (Spawning)
+	{
+
+		if (!Spawners[IndexSpawn])
+			GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Red, "uninitialized spawner:" + IndexSpawn);
+
+		Manager.Add(GetWorld()->SpawnActor<ABaseEnemy>(TrashMob->GeneratedClass, Spawners[IndexSpawn]->GetActorLocation(),
+		Spawners[IndexSpawn]->GetActorRotation(), SpawnParams));
+		NumberMinionToSpawnCurr++;
+		IndexSpawn++;
+		if(IndexSpawn > Manager.Num()-1)
+			IndexSpawn = 0;
+
+		if(NumberMinionToSpawnCurr == NumberMinionToSpawn)
+		{
+			Spawning = false;
+			NumberMinionToSpawnCurr = 0;
+		}
+	}
+}
