47a3cd36577263e8978210765bc1a64d7eaf2a1d
Jonathan <j.six@student.isartdigital.com>
2020-10-28 18:13:20 +0100

    Add fireBall patterns

---
+++ Source/GoodManStory/Character/FireBall.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireBall.h"
+
+// Sets default values
+AFireBall::AFireBall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFireBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
